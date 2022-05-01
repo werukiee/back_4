264b7913097fa30ee632fe7d93002bbdbdd9cde7
werukie <79107962+werukiee@users.noreply.github.com>
2022-05-01 17:49:14 +0300

    wow

---
+++ form.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Anastasia</title>
+</head>
+
+<body>
+    <div class="block_form" id="form">
+        <div class="top_bot">
+            <h1>Form</h1>
+            <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" id="form">
+                <div pl-2 class="form-row">
+                    <label>Имя:</label>
+                    <input class="form-control <?php if (!empty($nameErr)) {
+                                                    echo 'bg-danger text-light is-invalid';
+                                                } else {
+                                                    echo 'is-valid';
+                                                } ?>" <?php
+                                                        if (isset($_COOKIE['name'])) {
+                                                            echo 'value="' . $_COOKIE['name'] . '"';
+                                                        } ?> type="text" name="field-name" class="form-control" required>
+                    <?php
+                    if (!empty($nameErr)) {
+                        echo '<span class="text-danger">' . $nameErr . '</span>';
+                    }
+                    ?>
+                </div>
+                <div class="form-group">
+                    <label>Поле email:</label>
+                    <input class="form-control <?php if (!empty($emailErr)) {
+                                                    echo 'bg-danger text-light is-invalid';
+                                                } else {
+                                                    echo 'is-valid';
+                                                } ?>" <?php
+                                                        if (isset($_COOKIE['email'])) {
+                                                            echo 'value="' . $_COOKIE['email'] . '"';
+                                                        } ?> name="field-email" type="email" required>
+                    <?php
+                    if (!empty($emailErr)) {
+                        echo '<span class="text-danger">' . $emailErr . '</span>';
+                    }
+                    ?>
+                </div>
+
+                <label>Дата рождения:</label>
+                <input class="form-control" name="field-date" value="2012-12-12" type="date" required />
+                
+                <label>Пол:</label>
+                <br />
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" checked="checked" id="genderRadio1" name="radio-gender" value=1 class="custom-control-input" />
+                    <label class="custom-control-label" for="genderRadio1">Мужской</label>
+                </div>
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" id="genderRadio2" name="radio-gender" value=0 class="custom-control-input" />
+                    <label class="custom-control-label" for="genderRadio1">Женский</label>
+                </div>
+                <br />
+
+                <label>Кол-во конечностей:</label>
+                <br />
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" checked="checked" id="konechRadio0" name="radio-konech" value=0 class="custom-control-input" />
+                    <label class="custom-control-label" for="konechRadio0">0</label>
+                </div>
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" checked="checked" id="konechRadio1" name="radio-konech" value=1 class="custom-control-input" />
+                    <label class="custom-control-label" for="konechRadio1">1</label>
+                </div>
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" checked="checked" id="konechRadio2" name="radio-konech" value=2 class="custom-control-input" />
+                    <label class="custom-control-label" for="konechRadio2">2</label>
+                </div>
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" checked="checked" id="konechRadio3" name="radio-konech" value=3 class="custom-control-input" />
+                    <label class="custom-control-label" for="konechRadio3">3</label>
+                </div>
+                <div class="custom-control custom-radio form-check-inline">
+                    <input type="radio" checked="checked" id="konechRadio4" name="radio-konech" value=4 class="custom-control-input" />
+                    <label class="custom-control-label" for="konechRadio4">4</label>
+                </div>
+                <br />
+
+                <label>Ваши сверхспособности:</label>
+                <br />
+                <select class="custom-select" name="superpower[]" required>
+                    <option value="Бессмертие">Бессмертие</option>
+                    <option value="Прохождение сквозь стены">Прохождение сквозь стены</option>
+                    <option value="Левитация">Левитация</option>
+                </select>
+                <br />
+
+                <label>Биография:</label>
+                <br />
+                <div class="w-100">
+                <textarea name="biography" <?php if (!empty($bioErr)) {
+                                                echo 'class="placeholder col-12 bg-danger text-light is-invalid"';
+                                            } else {
+                                                echo 'class="is-valid"';
+                                            } ?>></textarea></div>
+
+                <?php
+                if (!empty($bioErr)) {
+                    echo '<span class="text-danger">' . $bioErr . '</span>';
+                }
+                ?>
+                <br />
+
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input <?php if(!empty($chickErr)) { echo 'is-invalid';} else { echo 'is-valid'; }?>" name="chick" id="customCheck1" checked=checked value=1 <?php if (!empty($chickErr)) {
+                                                                                                                                    echo 'class="text-danger"';
+                                                                                                                                } ?>>
+                    <label class="custom-control-label" for="customCheck1">Ознакомлен с контрактом</label>
+                </div>
+
+                <?php
+                if (!empty($chickErr)) {
+                    echo '<span class="text-danger">' . $chickErr . '</span>';
+                }
+                ?>
+                <input type="submit" value="Отправить" class="btn btn-primary text-dark" />
+            </form>
+        </div>
+    </div>
+</body>
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8'); 
+
+$nameErr = $emailErr = $bioErr = $chickErr="";
+
+$result;
+
+try{
+    $errors = FALSE;
+    if(isset($_GET['field-name'])){
+    $name = $_GET['field-name'];
+    $email = $_GET['field-email'];
+    $data = $_GET['field-date'];
+    $gender = $_GET['radio-gender'];
+    $konech = $_GET['radio-konech'];
+    $bio = $_GET['biography'];
+    
+    if (empty($name)) {
+        $nameErr = "Введите имя";
+        $errors = TRUE;
+    }
+    else 
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+            $nameErr = "Некорректно введено имя"; 
+            $errors = TRUE;
+        }
+        else{
+            setcookie('name',$name,time()+365*24*60*60);
+        }
+    if (empty($email)) {
+        $emailErr = "Введите Email";
+        $errors = TRUE;
+    }
+    else 
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Некорректно введён email";
+            $errors = TRUE;
+        }
+        else{
+            setcookie('email',$email,time()+365*24*60*60);
+        }
+
+    if (empty($bio)) {
+        $bioErr = "Введите биографию";
+        $errors = TRUE;
+    }
+    else{
+        setcookie('bio',$bio,time()+365*24*60*60);
+    }
+    if (empty($_GET['chick'])) {
+        $chickErr = "Чтобы продолжить, нужно согласиться с условиями";
+        $errors = TRUE;
+    }
+    
+    if (!$errors) {
+        $sup= implode(",",$_GET['superpower']);
+        $chebox = $_GET['chick'];
+
+        $conn = new PDO("mysql:host=localhost;dbname=u41810", 'u41810', '3516685', array(PDO::ATTR_PERSISTENT => true));
+
+        $user = $conn->prepare("INSERT INTO form SET id = ?,name = ?, email = ?, data = ?, gender = ?, konech = ?, bio = ?, chebox = ?");
+
+        $id_user = $conn->lastInsertId();
+        $user -> execute([$id_user, $_GET['field-name'], $_GET['field-email'], date('Y-m-d', strtotime($_GET['field-date'])), $_GET['radio-gender'], $_GET['radio-konech'], $_GET['biography'], $_GET['chick']]);
+
+        $user1 = $conn->prepare("INSERT INTO super SET id = ?, super_name = ?");
+        $user1 -> execute([$id_user, $sup]);
+        $result = true;
+    }
+}
+}
+catch(PDOException $e){
+    echo "$e";
+    print('Error : ' . $e->getMessage());
+    exit();
+}
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+    if (isset($result) && $result) {
+        include('success.php');
+    } else {
+        include('form.php'); // Включаем содержимое файла form.php.
+    }
+    
+    exit();
+}
+
+?>
